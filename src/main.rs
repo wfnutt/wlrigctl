a33109f062176d971bbed09717d74bdb8733cf5a
Martin Brodbeck <martin@brodbeck-online.de>
2023-11-29 13:11:26 +0100

    simplify error handling

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,8 @@ mod settings;
 use settings::Settings;
 
 fn main() {
-    let settings = Settings::new();
-    let settings = match settings {
-        Ok(s) => s,
-        Err(_) => {
-            println!("Could not read settings. Please provide the settings file 'clrigctl.toml'.");
-            return;
-        }
-    };
+    let settings = Settings::new()
+        .expect("Could not read settings.");
 
     println!("{:?}", settings);
 }
