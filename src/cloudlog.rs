244fc759eaf1911e3bb2978928a261f5686691e5
Martin Brodbeck <martin@brodbeck-online.de>
2023-11-29 20:00:05 +0100

    code beutifying

--- src/cloudlog.rs
+++ src/cloudlog.rs
@@ -1,16 +1,21 @@
-use serde::Serialize;
 use reqwest::Client;
+use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct RadioData {
     pub key: String,
     pub radio: String,
-    pub frequency : String,
+    pub frequency: String,
     pub mode: String,
     pub power: String,
 }
 
 #[tokio::main]
 pub async fn upload(url: &str, radio_data: &RadioData) {
-    let _res = Client::new().post(url).json(&radio_data).send().await.unwrap();
+    let _res = Client::new()
+        .post(url)
+        .json(&radio_data)
+        .send()
+        .await
+        .unwrap();
 }
